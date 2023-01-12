8052b974e80dc7c94f058db4e479275b0f0fe0f2
Kelvin Mwenda <NKMEKE1964@gmail.com>
2023-01-12 18:04:20 +0000

    min opp

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''Minimum Operations task'''
+
+
+def minOperations(n):
+    '''write a method that calculates the fewest number of operations 
+ needed to result in exactly n H characters in the file.
+    Returns:
+        Integer : if n is impossible to achieve, return 0
+    '''
+    pasted = 1  # how many characters in file
+    copied = 0  # how many H's copied
+    counter = 0  # operations counter
+
+    while pasted_chars < n:
+        # if did not copy anything yet
+        if copied == 0:
+            # copy all
+            copied = pasted
+            # increment operations counter
+            counter += 1
+
+        # if haven't pasted anything yet
+        if pasted == 1:
+            # paste
+            pasted += copied
+            # increment operations counter
+            counter += 1
+            # continue to next loop
+            continue
+
+        remaining = n - pasted  # remaining chars to Paste
+        
+        if remaining < copied:
+            return 0
+
+        # if can't be devided
+        if remaining % pasted != 0:
+            # paste copied
+            pasted += copied
+            # increment operations counter
+            counter += 1
+        else:
+            # copyall
+            copied = pasted
+            # paste
+            pasted += copy
+            # increment operations counter
+            counter += 2
+
+    # if got the desired result
+    if pasted == n:
+        return counter
+    else:
+        return 0
