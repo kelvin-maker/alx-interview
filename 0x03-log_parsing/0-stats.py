9df66d5d3ff3664001cb81cab231d2c0f0c6dd6b
kelvin mwenda <62829439+kelvin-maker@users.noreply.github.com>
2023-02-14 09:42:47 +0300

    redid

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
-"""
-a script that reads stdin line by line and computes metrics:
-"""
 
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 counter = 0
 
-for line in sys.stdin:
-    try:
-        elements = line.split()
-        ip, date, request, code, size = elements[0], elements[2],
-        elements[5], int(elements[7]), int(elements[8])
-        if code in status_codes:
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
             total_size += size
-            status_codes[code] += 1
             counter += 1
-            if counter == 10:
-                print("File size: {}".format(total_size))
-                for status in sorted(status_codes):
-                    if status_codes[status]:
-                        print("{}: {}".format(status, status_codes[status]))
-                counter = 0
-    except ValueError:
-        pass
 
-print("File size: {}".format(total_size))
-for status in sorted(status_codes):
-    if status_codes[status]:
-        print("{}: {}".format(status, status_codes[status]))
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,58 +1,91 @@
 #!/usr/bin/python3
+'''N Queens Challenge'''
+
 import sys
-"""
-The N queens puzzle is the challenge of placing N non-attacking queens
-on an N×N chessboard. Write a program that solves the N queens problem.
-"""
-
-
-def nqueens(n):
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    def is_valid(board, row, col):
-        # Check if there is a queen in the same column
-        for i in range(row):
-            if board[i] == col:
-                return False
-
-        # Check diagonals
-        for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-            if board[i] == j:
-                return False
-        for i, j in zip(range(row, -1, -1), range(col, n)):
-            if board[i] == j:
-                return False
-
-        return True
-
-    def solve(board, row):
-        if row == n:
-            result.append(board[:])
-            return
-
-        for col in range(n):
-            if is_valid(board, row, col):
-                board[row] = col
-                solve(board, row + 1)
-
-    result = []
-    board = [-1] * n
-    solve(board, 0)
-    return result
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+
     try:
         n = int(sys.argv[1])
     except ValueError:
-        print("N must be a number")
-        sys.exit(1)
+        print('N must be a number')
+        exit(1)
 
-    result = nqueens(n)
-    for solution in result:
-        print([[i, j] for i, j in enumerate(solution)])
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
+
+    solutions = []
+    placed_queens = []  # coordinates format [row, column]
+    stop = False
+    r = 0
+    c = 0
+
+    # iterate thru rows
+    while r < n:
+        goback = False
+        # iterate thru columns
+        while c < n:
+            # check is current column is safe
+            safe = True
+            for cord in placed_queens:
+                col = cord[1]
+                if(col == c or col + (r-cord[0]) == c or
+                        col - (r-cord[0]) == c):
+                    safe = False
+                    break
+
+            if not safe:
+                if c == n - 1:
+                    goback = True
+                    break
+                c += 1
+                continue
+
+            # place queen
+            cords = [r, c]
+            placed_queens.append(cords)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if r == n - 1:
+                solutions.append(placed_queens[:])
+                for cord in placed_queens:
+                    if cord[1] < n - 1:
+                        r = cord[0]
+                        c = cord[1]
+                for i in range(n - r):
+                    placed_queens.pop()
+                if r == n - 1 and c == n - 1:
+                    placed_queens = []
+                    stop = True
+                r -= 1
+                c += 1
+            else:
+                c = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if goback:
+            r -= 1
+            while r >= 0:
+                c = placed_queens[r][1] + 1
+                del placed_queens[r]  # delete previous queen coordinates
+                if c < n:
+                    break
+                r -= 1
+            if r < 0:
+                break
+            continue
+        r += 1
+
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
